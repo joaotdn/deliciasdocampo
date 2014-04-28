597dc1ee147f5c68c533a2018be279b5002aead8
Joao Teodoro <joaotdn@gmail.com>
2014-04-28 14:23:45 -0300

    html theme update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+$('.the-content').perfectScrollbar({
+	suppressScrollX : true,
+	wheelSpeed: 20,
+  	wheelPropagation: false
+});
+
+$.each($('a','#nav'), function() {
+	$(this).text('');
+});
+
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
